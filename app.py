e2dafe2bc7a75ebf104facc3d62cd5a12c0a6878
Priyankasinthoju <86578034+Priyankasinthoju@users.noreply.github.com>
2021-10-05 23:10:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,11 +18,8 @@ def Home():
 standard_to = StandardScaler()
 @app.route("/predict", methods = ['POST'])
 def predict():
-    alert_message = False
-    success_message = False
     try:
         if request.method == 'POST':
-            features = request.form.to_dict()
 
             FirstName = request.form['FirstName']
             LastName = request.form['LastName']
@@ -94,15 +91,14 @@ def predict():
             
             prediction=model.predict([[CreditAmount,Age,Rpay_Status_1,Rpay_Status_2,Rpay_Status_3,Rpay_Status_4,Rpay_Status_5,Rpay_Status_6,Statement_1,Statement_2,Statement_3,Statement_4,Statement_5,Statement_6,Payment_1,Payment_2,Payment_3,Payment_4,Payment_5,Payment_6,Gender_Male,Gender_Female,Education_Graduate,Education_University,Education_HighSchool,Education_Others,MaritalStatus_Married,MaritalStatus_Single,MaritalStatus_Others]])
             if prediction[0] == 1:
-                alert_message = "This account will be defaulted"
+                return render_template('webpageupdated.html',prediction_texts="This account will be defaulted")
             else:
-                success_message = "This account will not be defaulted"
+                return render_template('webpageupdated.html',prediction_texts="This account will be Not defaulted")
 
             Default_pay = int(prediction[0])
             dbHandler.insertUser(FirstName,LastName,Email,Education,Age,SSN,PhoneNumber,Gender,MaritalStatus,CreditAmount,Rpay_Status_1,Rpay_Status_2,Rpay_Status_3,Rpay_Status_4,Rpay_Status_5,Rpay_Status_6,Statement_1,Statement_2,Statement_3,Statement_4,Statement_5,Statement_6,Payment_1,Payment_2,Payment_3,Payment_4,Payment_5,Payment_6,Default_pay)
     except:
-        alert_message = "Please enter relevant information."
-    return render_template('webpageupdated.html',alert_message = alert_message, success_message = success_message)
+    return render_template('webpageupdated.html',prediction_texts="Please enter relevant information.")
         
 
 if __name__ == "__main__":
