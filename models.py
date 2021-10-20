0927b615d214c2d91db51a768227929e858820ef
Priyankasinthoju <86578034+Priyankasinthoju@users.noreply.github.com>
2021-10-19 23:34:27 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import sqlite3 as sql
 import os
-currentdirectory = os.path.dirname(os.path.abspath(__file__))
+currentdirectory = os.path.dirname(os.path.abspath('DATABASE_URL'))
 def insertUser(FirstName,LastName,Email,Education,DateOfBirth,SSN,PhoneNumber,Gender,MaritalStatus,CreditAmount,Rpay_Status_1,Rpay_Status_2,Rpay_Status_3,Rpay_Status_4,Rpay_Status_5,Rpay_Status_6,Statement_1,Statement_2,Statement_3,Statement_4,Statement_5,Statement_6,Payment_1,Payment_2,Payment_3,Payment_4,Payment_5,Payment_6,Default_pay):
     con = sql.connect("Customer_Information.db")
     cur = con.cursor()
